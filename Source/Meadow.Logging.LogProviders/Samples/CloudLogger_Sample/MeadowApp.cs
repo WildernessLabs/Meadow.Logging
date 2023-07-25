2461714315cdc152c9264dffd85d1491f777a52e
Brian Kim <brian.kim@wildernesslabs.co>
2023-07-25 08:58:18 -0700

    auto wifi + ntp

--- Source/Meadow.Logging.LogProviders/Samples/CloudLogger_Sample/MeadowApp.cs
+++ Source/Meadow.Logging.LogProviders/Samples/CloudLogger_Sample/MeadowApp.cs
@@ -11,37 +11,14 @@ public class MeadowApp : App<F7FeatherV2>
     {
         Resolver.Log.Info($"Initializing...");
         Resolver.Log.AddProvider(new CloudLogger());
-        
-        await ConnectWifi();
 
+        // make sure to set creds in wifi.config.yaml
+        
         var count = 1;
         while (true)
         {
-            Resolver.Log.Info($"log loop {count++}");
+            Resolver.Log.Info($"log loop {count++} - {DateTime.UtcNow}");
             await Task.Delay(60 * 1000);
-        }
-    }
-    
-    private async Task ConnectWifi()
-    {
-        var SSID = "ssid";
-        var PASSCODE = "pass";
-
-        Resolver.Log.Info("Connecting to network...");
-
-        try
-        {
-            var wifi = Device.NetworkAdapters.Primary<IWiFiNetworkAdapter>();
-            await wifi.Connect(SSID, PASSCODE);
-
-            wifi.NetworkConnected += async (s, e) =>
-            {
-                Resolver.Log.Info("Network connected");
-            };
-        }
-        catch (Exception ex)
-        {
-            Resolver.Log.Error($"Error connecting to network: {ex.Message}");
         }
     }
 }
