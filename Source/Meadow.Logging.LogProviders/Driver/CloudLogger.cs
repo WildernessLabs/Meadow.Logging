7bf059671a53a7534c3dfacd9156bdc1bb7dee43
Brian Kim <brian.kim@wildernesslabs.co>
2023-07-25 14:04:39 -0700

    event changes

--- Source/Meadow.Logging.LogProviders/Driver/CloudLogger.cs
+++ Source/Meadow.Logging.LogProviders/Driver/CloudLogger.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.Json;
 using System.Threading;
+using System.Threading.Tasks;
 using Meadow.Cloud;
 
 namespace Meadow.Logging;
@@ -24,9 +27,16 @@ public class CloudLogger : ILogProvider
         {
             File.Create(LogFilePath).Close();
         }
+        
+        EventFilePath = Path.Combine(Resolver.Device.PlatformOS.FileSystem.DocumentsDirectory, "events.log");
+        if (!File.Exists(EventFilePath))
+        {
+            File.Create(EventFilePath).Close();
+        }
     }
 
     public string LogFilePath { get; protected set; }
+    public string EventFilePath { get; protected set; }
     public LogLevel MinLevel { get; protected set; }
     private char delim = '|';
     static SemaphoreSlim semaphoreSlim = new SemaphoreSlim(1, 1);
@@ -35,81 +45,83 @@ public class CloudLogger : ILogProvider
     {
         if (level >= MinLevel)
         {
-            Resolver.Log.Debug($"cloudlogger.log called");
-            
             var cloudLog = new CloudLog()
             {
-                Level = level.ToString(),
+                Severity = level.ToString(),
                 Message = message,
                 Timestamp = DateTime.UtcNow
             };
             
-            var connected = Resolver.Device.NetworkAdapters.Any(a => a.IsConnected);
-
-            if (connected)
-            {
-                await semaphoreSlim.WaitAsync();
-
-                try
-                {
-                    // send messages that were stored offline
-                    var lines = File.ReadAllLines(LogFilePath);
-                    if (lines.Length > 0)
-                    {
-                        Resolver.Log.Debug($"processing {lines.Length} stored cloud logs");
-                        foreach (var line in lines)
-                        {
-                            if (string.IsNullOrWhiteSpace(line))
-                            {
-                                continue;
-                            }
-
-                            var parsed = ParseLine(line);
-                            if (parsed != null)
-                            {
-                                await Resolver.MeadowCloudService.SendLog(parsed);
-                            }
-                        }
-
-                        File.Create(LogFilePath).Close();
-                        Resolver.Log.Debug($"cleared stored cloud logs");
-                    }
-
-                    // send current message
-                    Resolver.Log.Debug($"sending log");
-                    await Resolver.MeadowCloudService.SendLog(cloudLog);
-                }
-                catch (Exception ex)
-                {
-                    Resolver.Log.Debug($"error sending cloud log: {ex.Message}");
-                }
-                finally
-                {
-                    semaphoreSlim.Release();
-                }
-            }
-            else
-            {
-                Resolver.Log.Debug("writing cloud log to store");
-                File.AppendAllLines(LogFilePath, 
-                    new[] { $"{cloudLog.Level}{delim}{cloudLog.Timestamp}{delim}{cloudLog.Message}" });
-            }
+            Send(LogFilePath, cloudLog, Resolver.MeadowCloudService.SendLog);
         }
     }
 
-    private CloudLog? ParseLine(string line)
+    public async void LogEvent(int eventId, string description, Dictionary<string, object> measurements)
     {
-        var cloudLog = new CloudLog();
-        
-        var parts = line.Split(delim);
-        if (parts.Length >= 3)
+        var cloudEvent = new CloudEvent()
         {
-            cloudLog.Level = parts[0];
-            cloudLog.Timestamp = DateTime.Parse(parts[1]);
-            cloudLog.Message = string.Join(delim, parts.Skip(2).Take(parts.Length - 2));
-            return cloudLog;
-        }
+            EventId = eventId,
+            Description = description,
+            Measurements = measurements,
+            Timestamp = DateTime.UtcNow
+        };
+        
+        Send(EventFilePath, cloudEvent, Resolver.MeadowCloudService.SendEvent);
+    }
 
-        return null;
+    private async void Send<T>(string file, T item, Func<T, Task> sendFunc)
+    {
+        var serializeOptions = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+        
+        var connected = Resolver.Device.NetworkAdapters.Any(a => a.IsConnected);
+
+        if (connected)
+        {
+            await semaphoreSlim.WaitAsync();
+
+            try
+            {
+                // send messages that were stored offline
+                var lines = File.ReadAllLines(file);
+                if (lines.Length > 0)
+                {
+                    Resolver.Log.Debug($"processing {lines.Length} stored {typeof(T)}");
+                    foreach (var line in lines)
+                    {
+                        if (string.IsNullOrWhiteSpace(line))
+                        {
+                            continue;
+                        }
+
+                        var o = JsonSerializer.Deserialize<T>(line, serializeOptions);
+                        if (o != null)
+                        {
+                            await sendFunc(o);
+                        }
+                    }
+
+                    File.Create(file).Close();
+                    Resolver.Log.Debug($"cleared stored {typeof(T)}");
+                }
+
+                // send current message
+                Resolver.Log.Debug($"sending {typeof(T)}");
+                await sendFunc(item);
+            }
+            catch (Exception ex)
+            {
+                Resolver.Log.Debug($"error sending {typeof(T)}: {ex.Message}");
+            }
+            finally
+            {
+                semaphoreSlim.Release();
+            }
+        }
+        else
+        {
+            var json = JsonSerializer.Serialize(item, serializeOptions);
+            File.AppendAllLines(file, new[] { json });
+            Resolver.Log.Debug($"saved cloud log to local store {json}");
+        }
     }
 }
--- Source/Meadow.Logging.LogProviders/Samples/CloudLogger_Sample/MeadowApp.cs
+++ Source/Meadow.Logging.LogProviders/Samples/CloudLogger_Sample/MeadowApp.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Meadow.Devices;
 using Meadow.Hardware;
@@ -10,14 +11,24 @@ public class MeadowApp : App<F7FeatherV2>
     public override async Task Initialize()
     {
         Resolver.Log.Info($"Initializing...");
-        Resolver.Log.AddProvider(new CloudLogger());
+        var cloudLogger = new CloudLogger();
+        Resolver.Log.AddProvider(cloudLogger);
+        Resolver.Services.Add(cloudLogger);
 
         // make sure to set creds in wifi.config.yaml
-        
+
         var count = 1;
+        Random r = new Random();
         while (true)
         {
-            Resolver.Log.Info($"log loop {count++} - {DateTime.UtcNow}");
+            Resolver.Log.Info($"log loop {count++}");
+            
+            // var cl = Resolver.Services.Get<CloudLogger>();
+            // cl.LogEvent(0, "my first event", new Dictionary<string, object>()
+            // {
+            //     { "temperature", r.Next(80, 110) },
+            //     { "city", "log angeles" }
+            // });
             await Task.Delay(60 * 1000);
         }
     }
