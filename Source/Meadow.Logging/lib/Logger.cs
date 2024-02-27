0c6b0d98bd52bff860349b290645bb9ccd668dd5
Adrian Stevens <adrian.stevens@gmail.com>
2024-02-27 12:19:30 -0800

    Release 1.9.0

--- Source/Meadow.Logging/lib/Logger.cs
+++ Source/Meadow.Logging/lib/Logger.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Meadow.Logging;
 
@@ -7,9 +8,43 @@ namespace Meadow.Logging;
 /// </summary>
 public class Logger
 {
-    private LogProviderCollection _providers = new LogProviderCollection();
+    /// <summary>
+    /// Defines message groups for categorizing log messages.
+    /// </summary>
+    public static class MessageGroup
+    {
+        /// <summary>
+        /// Represents all log messages.
+        /// </summary>
+        public const string All = "all";
+
+        /// <summary>
+        /// Represents the operating system-related log messages.
+        /// </summary>
+        public const string OS = "os";
+
+        /// <summary>
+        /// Represents the core system-related log messages.
+        /// </summary>
+        public const string Core = "core";
+
+        /// <summary>
+        /// Represents the application-specific log messages.
+        /// </summary>
+        public const string Application = "application";
+    }
+
+    private LogProviderCollection _providers = new();
 
     private readonly int _startupTicks;
+
+    /// <summary>
+    /// Gets the list of groups used for displaying log messages.
+    /// </summary>
+    /// <remarks>
+    /// To show message from all groups, either use `MessageGroup.All` <i>or</i> clear the ShowsGroups list
+    /// </remarks>
+    public List<string> ShowGroups { get; } = new();
 
     /// <summary>
     /// Gets or sets a value indicating whether to show ticks in log messages
@@ -27,6 +62,8 @@ public class Logger
     /// <param name="providers">A collection of providers to add to the Logger</param>
     public Logger(params ILogProvider[] providers)
     {
+        ShowGroups.Add(MessageGroup.Application);
+
         foreach (var p in providers)
         {
             AddProvider(p);
@@ -66,9 +103,10 @@ public class Logger
     /// Sends a Trace-level message to all ILogProviders
     /// </summary>
     /// <param name="message">The message to send to Providers</param>
-    public void Trace(string message)
+    /// <param name="messageGroup">The (optional) message group</param>
+    public void Trace(string message, string? messageGroup = null)
     {
-        Log(LogLevel.Trace, message);
+        Log(LogLevel.Trace, message, messageGroup);
     }
 
     /// <summary>
@@ -76,18 +114,20 @@ public class Logger
     /// </summary>
     /// <param name="condition">The message will be sent to Providers only when this is true</param>
     /// <param name="message">The message to send to Providers</param>
-    public void TraceIf(bool condition, string message)
+    /// <param name="messageGroup">The (optional) message group</param>
+    public void TraceIf(bool condition, string message, string? messageGroup = null)
     {
-        if (condition) Log(LogLevel.Trace, message);
+        if (condition) Log(LogLevel.Trace, message, messageGroup);
     }
 
     /// <summary>
     /// Sends a Debug-level message to all ILogProviders
     /// </summary>
     /// <param name="message">The message to send to Providers</param>
-    public void Debug(string message)
+    /// <param name="messageGroup">The (optional) message group</param>
+    public void Debug(string message, string? messageGroup = null)
     {
-        Log(LogLevel.Debug, message);
+        Log(LogLevel.Debug, message, messageGroup);
     }
 
     /// <summary>
@@ -95,18 +135,20 @@ public class Logger
     /// </summary>
     /// <param name="condition">The message will be sent to Providers only when this is true</param>
     /// <param name="message">The message to send to Providers</param>
-    public void DebugIf(bool condition, string message)
+    /// <param name="messageGroup">The (optional) message group</param>
+    public void DebugIf(bool condition, string message, string? messageGroup = null)
     {
-        if (condition) Log(LogLevel.Debug, message);
+        if (condition) Log(LogLevel.Debug, message, messageGroup);
     }
 
     /// <summary>
     /// Sends an Info-level message to all ILogProviders
     /// </summary>
     /// <param name="message">The message to send to Providers</param>
-    public void Info(string message)
+    /// <param name="messageGroup">The (optional) message group</param>
+    public void Info(string message, string? messageGroup = null)
     {
-        Log(LogLevel.Information, message);
+        Log(LogLevel.Information, message, messageGroup);
     }
 
     /// <summary>
@@ -114,18 +156,20 @@ public class Logger
     /// </summary>
     /// <param name="condition">The message will be sent to Providers only when this is true</param>
     /// <param name="message">The message to send to Providers</param>
-    public void InfoIf(bool condition, string message)
+    /// <param name="messageGroup">The (optional) message group</param>
+    public void InfoIf(bool condition, string message, string? messageGroup = null)
     {
-        if (condition) Log(LogLevel.Information, message);
+        if (condition) Log(LogLevel.Information, message, messageGroup);
     }
 
     /// <summary>
     /// Sends a Warn-level message to all ILogProviders
     /// </summary>
     /// <param name="message">The message to send to Providers</param>
-    public void Warn(string message)
+    /// <param name="messageGroup">The (optional) message group</param>
+    public void Warn(string message, string? messageGroup = null)
     {
-        Log(LogLevel.Warning, message);
+        Log(LogLevel.Warning, message, messageGroup);
     }
 
     /// <summary>
@@ -133,27 +177,30 @@ public class Logger
     /// </summary>
     /// <param name="condition">The message will be sent to Providers only when this is true</param>
     /// <param name="message">The message to send to Providers</param>
-    public void WarnIf(bool condition, string message)
+    /// <param name="messageGroup">The (optional) message group</param>
+    public void WarnIf(bool condition, string message, string? messageGroup = null)
     {
-        if (condition) Log(LogLevel.Warning, message);
+        if (condition) Log(LogLevel.Warning, message, messageGroup);
     }
 
     /// <summary>
     /// Sends a Error-level message to all ILogProviders
     /// </summary>
     /// <param name="exception">The exception to translate and send to Providers</param>
-    public void Error(Exception exception)
+    /// <param name="messageGroup">The (optional) message group</param>
+    public void Error(Exception exception, string? messageGroup = null)
     {
-        Log(LogLevel.Error, exception.ToString());
+        Log(LogLevel.Error, exception.ToString(), messageGroup);
     }
 
     /// <summary>
     /// Sends a Error-level message to all ILogProviders
     /// </summary>
     /// <param name="message">The message to send to Providers</param>
-    public void Error(string message)
+    /// <param name="messageGroup">The (optional) message group</param>
+    public void Error(string message, string? messageGroup = null)
     {
-        Log(LogLevel.Error, message);
+        Log(LogLevel.Error, message, messageGroup);
     }
 
     /// <summary>
@@ -161,14 +208,26 @@ public class Logger
     /// </summary>
     /// <param name="condition">The message will be sent to Providers only when this is true</param>
     /// <param name="message">The message to send to Providers</param>
-    public void ErrorIf(bool condition, string message)
+    /// <param name="messageGroup">The (optional) message group</param>
+    public void ErrorIf(bool condition, string message, string? messageGroup = null)
     {
-        if (condition) Log(LogLevel.Error, message);
+        if (condition) Log(LogLevel.Error, message, messageGroup);
     }
 
-    private void Log(LogLevel level, string message)
+    private void Log(LogLevel level, string message, string? messageGroup)
     {
         if (LogLevel > level) return;
+
+        if (messageGroup == null) messageGroup = MessageGroup.Application;
+
+        if (ShowGroups.Count > 0)
+        {
+            if (!ShowGroups.Contains(MessageGroup.All)
+                && !ShowGroups.Contains(messageGroup))
+            {
+                return;
+            }
+        }
 
         TimeSpan? now = null;
 
@@ -186,7 +245,7 @@ public class Logger
                     message = $"[+{now:h\\:m\\:ss\\.FFF}] {message}";
                 }
 
-                p.Log(level, message);
+                p.Log(level, message, messageGroup);
             }
         }
     }
